62f918dcf80cdac8c8291886d478e991154b01de
BoStack <Beau@qrspace.io>
2022-09-12 13:02:54 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-*main fuction - print the output string of put fuction
-8
-*return :0
+*main function - print the output string of puts fuction
+*
+*
+*Return :0
 */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
